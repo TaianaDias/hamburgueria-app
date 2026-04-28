3dfeb9cb172ad99eb728ddbd0d5619739eaf028d
TaianaDias <57508229+TaianaDias@users.noreply.github.com>
2026-04-28 14:46:15 -0300

    Adiciona registro rapido de desperdicio

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = "hamburgueria-app-v13";
+const CACHE_NAME = "hamburgueria-app-v14";
 const APP_SHELL = [
   "/",
   "/login.html",
   "/index.html",
   "/estoque.html",
+  "/desperdicio.html",
   "/reposicao-producao.html",
   "/compras.html",
   "/alertas-reposicao.html",
